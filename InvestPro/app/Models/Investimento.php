8a8a47d9e1a912b6763c419cb666e8c227681cd1
NickCampos03 <nicolasjdc45@gmail.com>
2025-11-28 11:15:45 -0300

    Finalização do Model Investimentos

--- InvestPro/app/Models/Investimento.php
+++ InvestPro/app/Models/Investimento.php
@@ -6,9 +6,75 @@ use Illuminate\Database\Eloquent\Model;
 
 class Investimento extends Model
 {
-    public $id;
-    public $valorAplicado;
-    public $dataInicio;
-    public $dataFim;
-    public $retornoPercentual;
-}
+    protected $fillable = [
+        'valorAplicado',
+        'dataInicio'
+        'dataFim'
+        'retornoPercentual'
+        'ativoId'
+    ];
+
+    protected $casts =[
+        'dataFim' => 'datetime',
+        'dataFim' => 'datetime',
+        'ativoId' => Ativo::class 
+    ];
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getValorAplicado(){
+        return $this->valorAplicado;
+    }
+
+    public function setValorAplicado(double $valorAplicado){
+        $this->valorAplicado = $valorAplicado;
+        return $this;
+    }
+
+    public function getDataInicio():DateTime{
+        return $this->dataInicio;
+
+    }
+
+    public function setDataInicio(DateTime $dataInicio):DateTime{
+        $this->dataInicio = $dataInicio;
+        return $this;
+    }
+
+    public function getDataFim():DateTime{
+        return $this->dataFim;
+
+    }
+
+    public function setDataFim(DateTime $dataFim):DateTime{
+        $this->dataFim = $dataFim;
+        return $this;
+    }
+
+    public function getRetornoPercentual(){
+        return $this->retornoPercentual;
+    }
+
+    public function setRetornoPercentual(double $retornoPercentual){
+        $this->retornoPercentual = $retornoPercentual;
+        return $this;
+    }
+
+    public function getAtivoId():Ativo{
+        return $this->ativoId;
+    }
+
+    public function setAtivoId():Ativo{
+        $this->ativoId = $ativoId;
+        return $this;
+    }
+ }
