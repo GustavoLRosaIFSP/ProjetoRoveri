e0aa81740b139bc17723d958c14161fa265d27aa
Felipe <127034748+Felipe-student@users.noreply.github.com>
2025-11-28 22:54:20 -0300

    Atualizações model/carteira e controller/carteiracontroller

--- InvestPro/app/Models/Carteira.php
+++ InvestPro/app/Models/Carteira.php
@@ -2,9 +2,56 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Carteira extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'valor_total',
+        'quantidade',
+        'user_id'
+    ];
+    public function usuario()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    public function investimentos()
+    {
+        return $this->hasMany(Investimento::class);
+    }
+    public function adicionarInvestimento(Investimento $investimento)
+    {
+        $this->investimentos()->save($investimento);
+
+        $this->valor_total += $investimento->valorAplicado;
+        $this->quantidade++;
+
+        $this->save();
+    }
+    public function removerInvestimento($id)
+    {
+        $invest = $this->investimentos()->find($id);
+
+        if (!$invest) {
+            return false;
+        }
+
+        $this->valor_total -= $invest->valorAplicado;
+        $this->quantidade--;
+
+        $this->save();
+
+        $invest->delete();
+        return true;
+    }
+    public function calcularRetornoTotal()
+    {
+        return $this->investimentos->sum(function ($inv) {
+            return $inv->valorAplicado * ($inv->retornoPercentual / 100);
+        });
+    }
 }
